a0d3705aa8071043ef94d5f8363d76a095495f78
David Kang <david@lekko.com>
2024-06-04 15:55:22 -0700

    Handle bisync failure on base gracefully (#1)
    * Handle bisync failure on base gracefully
    
    * Adjust messaging to be more helpful

--- get_diff.sh
+++ get_diff.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 git config --global user.email "108442683+lekko-app[bot]@users.noreply.github.com"
 git config --global user.name "lekko-app[bot]"
-echo "pre-sync health check"
 git reset --hard && git clean -fd
-echo "$(git fsck --full)"
 git checkout $1
 echo "pre-sync base"
-lekko bisync -r ~/lekko
+set +e
+if ! lekko bisync -r ~/lekko; then
+  # If bisync fails on base commit, ignore and try to proceed
+  # This might result in detecting "unnecessary" additions, but hopefully fix PR should help resolve that
+  echo "Warning: bisync failed on base ($1). The current state of your Lekko repository will be used as the base of changes."
+fi
+set -e
 git reset --hard && git clean -fd
 cd ~/lekko
 git checkout -b ${GITHUB_REPOSITORY}-base
