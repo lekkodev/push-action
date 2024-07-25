b8a7e41aa2ce0d50173414ce86463d7028f98bcb
Sergey Passichenko <serj.de.sudden@gmail.com>
2024-07-25 00:49:37 -0700

    monorepo support (#6)
    * monorepo support
    
    * fix

--- read_dot_lekko_base.sh
+++ read_dot_lekko_base.sh
@@ -4,11 +4,13 @@ cd ${GITHUB_WORKSPACE}
 if [[ "$1" != "0000000000000000000000000000000000000000" ]]; then
   git reset --hard && git clean -fd
   git checkout $1
+  cd ${PROJECT_PATH}
   if ! lekko conf; then
     # lekko conf failed - assume this means .lekko did not exist at base
     echo "Current change introduced Lekko"
     echo "new_lekko=true" >> $GITHUB_OUTPUT
   fi
+  cd ${GITHUB_WORKSPACE}
 else
   echo "Base points to null commit"
   echo "new_lekko=true" >> $GITHUB_OUTPUT
